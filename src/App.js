2dcaf2c60e80e67785048fa544e907315144b4d5
realroy <roiboon.c@ku.th>
2018-04-05 18:05:21 +0700

    Fix config.yml

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Firebase</h1>
         </header>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis assumenda porro iusto modi ad aperiam vitae voluptate saepe veniam aspernatur, exercitationem sed a aliquid fugit harum, adipisci dicta perspiciatis natus?
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
